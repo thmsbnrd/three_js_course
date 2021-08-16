f07a9e7f869119550600b869578d2d1e24fb6a69
Thomas BENARD <thomas0benard@gmail.com>
2021-08-16 11:22:04 +0200

    created red cube in a scene

--- 03_basics/exercice/script.js
+++ 03_basics/exercice/script.js
@@ -1,1 +1,8 @@
-console.log("Hello Three.js");
+//Scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.MeshBasicMaterial(geometry, material);
+scene.add(mesh);
